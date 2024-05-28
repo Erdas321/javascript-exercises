c4be4f6bfee428bdba0968804405be91f03ccfc8
Erdas321 <{103915466+Erdas321@users.noreply.github.com }+{Erdas321}@users.noreply.github>
2024-05-28 13:12:13 +0300

    Javascript Exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    let result = "";
+    for (let i = 0; i < times; i++) {
+        result += word;
+    }
+    return times < 0 ? "Error" : result;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let result = 0;
+    if (isNaN(num1) || isNaN(num2)) {
+        return "Error";
+    } else if (num1 < 0 || num2 < 0) {
+        return "Error";
+    } else {
+        result = (((num2 - num1) + 1) / 2) * (num2 + num1);
+    }
+    return result;
 };
 
 // Do not edit below this line
