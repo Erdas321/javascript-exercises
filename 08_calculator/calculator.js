bd0b17a683e145db8670156c6d00c09819d7ccef
Erdas321 <{103915466+Erdas321@users.noreply.github.com }+{Erdas321}@users.noreply.github>
2024-06-08 12:26:31 +0300

    All Exercises Complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if (arr.length === 0) {
+    return 0;
+  } else if (arr.length === 1) {
+    return arr[0];
+  }
+	return arr.reduce((a, b) => a + b);
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  return arr.reduce((a, b) => a * b);
 
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a === 0) {
+    return 1;
+  }
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let regex = /[^a-zA-Z0-9]/g;
+    string = string.toLowerCase();
+    let replacedString = string.replace(regex, "");
+    let reverseString = replacedString.split('').reverse().join('');
+    return replacedString === reverseString ? true : false;
 };
 
 // Do not edit below this line
